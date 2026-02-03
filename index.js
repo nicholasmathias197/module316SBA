83383fd89367cbaa996cfe4a7301869ce1b0d957
nicholasmathias197 <nicholas.mathias@peopleshores.com>
2026-02-03 08:55:31 -0500

    Project Structure and TODO list added

---
+++ index.js
@@ -0,0 +1,36 @@
+//TODO Cache at least one element using selectElementById.
+
+//TODO Cache at least one element using querySelector or querySelectorAll.
+
+//TODO Use the parent-child-sibling relationship to navigate between
+//elements at least once (firstChild, lastChild, parentNode, nextElementSibling, etc.).
+
+//TODO Iterate over a collection of elements to accomplish some task.
+
+//TODO Create at least one element using createElement.
+
+//TODO Use appendChild and/or prepend to add new elements to the DOM.
+
+//TODO Use the DocumentFragment interface or HTML templating with the cloneNode 
+// method to create templated content. 
+
+//TODO Modify the HTML or text content of at least one element in 
+// response to user interaction using innerHTML, innerText, or textContent.
+
+//TODO Modify the style and/or CSS classes of an element in 
+// response to user interactions using the style or classList properties.
+
+//TODO Modify at least one attribute of an element in response to user interaction.
+
+//TODO Register at least two different event listeners and create the associated event handler functions.
+
+//TODO Use at least two Browser Object Model (BOM) properties or methods.
+
+//TODO include at least one form and/or input with HTML attribute validation
+
+//TODO Include at least one form and/or input with DOM event-based validation. 
+// (This can be the same form or input as the one above, but should include event-based 
+// validation in addition to the HTML attribute validation.)
+
+
+
